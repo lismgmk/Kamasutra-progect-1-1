0d31f7d9e928d16b72e4e804e6acc68bad1d9b01
lismgmk <lismgmk2@gmail.com>
2021-04-05 02:35:48 +0300

    Select need little fix

--- src/Component/Select/Select.tsx
+++ src/Component/Select/Select.tsx
@@ -1,6 +1,6 @@
-import React, {MouseEvent, useState} from 'react';
+import React, {MouseEvent, useState, KeyboardEvent, useEffect} from 'react';
 import '../../App.css';
-import './select.css'
+import s from'./select.module.css'
 
 export type SelectType = {
     item: ItemType[]
@@ -13,30 +13,74 @@ type ItemType = {
 }
 
 export let Select = (props: SelectType) => {
-
-
-    const val = () => {
-        let k = props.item.find(i => i.title === nameValue)
-        return (k?.title)
-    }
-
     const [collapsed, setCollapsed] = useState<boolean>(false);
     const [nameValue, setNameValue] = useState<string>('Vova');
-    // const [nameNone, setNameNone] = useState<boolean>(false);
+
+
+
+    const val = props.item.find(i => i.title === nameValue);
+
+
+    const [hoverItem, setHoverItem] = useState(val);
+
+
+    const findMous = (m: string) => {
+        const val2 = props.item.find(i => i.value === m)
+        setHoverItem(val2)
+    }
+
+    // useEffect(()=>{
+    //     setHoverItem(val)}  , [props.item]
+    // )
+
+    const onKeyUp = (e : KeyboardEvent<HTMLDivElement> ) => {
+
+        if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+            for (let i = 0; i < props.item.length; i++) {
+                if (props.item[i] === hoverItem) {
+                    const pretendent = e.key === "ArrowDown"? props.item[i + 1] : props.item[i - 1];
+                    if (pretendent){
+                        setHoverItem(pretendent);
+                        break;
+                    }
+                }
+            }
+        }
+        if(e.key === "Enter"){
+
+            const val = props.item.find(i => i.title === e.currentTarget.innerHTML);
+            console.log(val);
+            setHoverItem(val);
+            setCollapsed(false);
+        }
+        if(e.key === "Escape"){
+            setCollapsed(false)
+        }
+
+    }
+
+
     return (
 
         <div>
             <div
-                className={'posRel'}
+                className={s.posRel}
                 onClick={() => {
                     setCollapsed(!collapsed)
-                }}>
-                {!collapsed && val()}
+                }}
+                tabIndex = {0}
+                onKeyUp={onKeyUp}
+            >
+                {hoverItem && hoverItem.title}
+
             </div>
-            <div className={collapsed ? 'posAbs' : ''}>
+            <div className={collapsed ? s.posAbs : ''}>
                 {collapsed && props.item.map((i) =>
+
                     <div
-                        className={'divHover'}
+                        key = {i.value}
+                        onMouseEnter={() => {findMous(i.value)}}
+                        className={`${s.divHover} ${hoverItem === i ? s.selected : ''}`}
                         id={i.value}
                         onClick={() => {
                             setNameValue(i.title)
@@ -53,4 +97,4 @@ export let Select = (props: SelectType) => {
     )
 
 
-};
+}
